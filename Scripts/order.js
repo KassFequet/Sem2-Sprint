2d7104e43b843543334d7290bae20c1a9156d9c3
CamB7 <cameronboyer1999@gmail.com>
2025-06-21 14:00:10 -0230

    finished order form and checkout box

--- Scripts/order.js
+++ Scripts/order.js
@@ -1,10 +1,9 @@
-// at the press of the locations button, show a list of locations under the the button in <p> with id locations
+// Show locations when the button is clicked
 document.addEventListener('DOMContentLoaded', function () {
   const locationsButton = document.getElementById('locationsButton');
   const locationsParagraph = document.getElementById('locations');
 
   locationsButton.addEventListener('click', function () {
-    // Toggle the visibility of the locations paragraph
     locationsParagraph.innerHTML =
       '<ul><li>123 Water St.</li><li>321 Bean Blvd.</li><li>789 Espresso Ave.</li></ul>';
   });
